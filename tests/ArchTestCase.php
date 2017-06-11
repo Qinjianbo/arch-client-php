72a9a9e74d4ed40426f0991fab5c9c817b15cb80
liugj <liugj@boqii.com>
2017-06-11 04:06:20 +0000

    去掉var_dump

--- tests/ArchTestCase.php
+++ tests/ArchTestCase.php
@@ -33,7 +33,6 @@ class ArchTestCase extends TestCase
         $s = 'price=price_app_v1&site_source=shop&brandid=0&cateid=622&attrid=&source=IOS&highlight=pname&facets=brandid%2Cc3%2Cp&format=json&range=price_app_v1%24%24';
         parse_str($s, $param);
         $response = $this->index->get($param);
-        var_dump($response);
         $this->assertArrayHasKey('list', $response);
         $this->assertArrayHasKey('total', $response);
     }
