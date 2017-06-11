e6e0634be28f89ce54b7309832f0501aa4321bb6
liugj <liugj@boqii.com>
2017-06-11 04:07:01 +0000

    keyword->q

--- tests/ArchTestCase.php
+++ tests/ArchTestCase.php
@@ -29,7 +29,7 @@ class ArchTestCase extends TestCase
 
     public function testSearch()
     {
-        $param = ['keyword' => '狗粮', 'cateid' => 621, 'source' => 'app'];
+        $param = ['q' => '狗粮', 'cateid' => 621, 'source' => 'app'];
         $s = 'price=price_app_v1&site_source=shop&brandid=0&cateid=622&attrid=&source=IOS&highlight=pname&facets=brandid%2Cc3%2Cp&format=json&range=price_app_v1%24%24';
         parse_str($s, $param);
         $response = $this->index->get($param);
